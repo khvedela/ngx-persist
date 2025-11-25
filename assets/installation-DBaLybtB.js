96dfc6b20d8108a2c50c594b86da44fa57c24a00
khvedela <khvedela@users.noreply.github.com>
2025-11-25 23:39:30 +0000

    deploy: 230e8a0fcd00378039b089b2e102e5c80dedc005

--- assets/installation-DBaLybtB.js
+++ assets/installation-DBaLybtB.js
@@ -2,9 +2,9 @@ const n=`---
 title: Installation
 ---
 
-<h1 id="installation-2">Installation</h1>
+<h1 id="installation-1">Installation</h1>
 <p>Install the library via npm:</p><pre><code class="language-bash"><span class="token function">npm</span> <span class="token function">install</span> ngx-persist
-</code></pre><h2 id="peer-dependencies-1">Peer Dependencies</h2>
+</code></pre><h2 id="peer-dependencies">Peer Dependencies</h2>
 <p><code>ngx-persist</code> requires Angular 19 or higher.</p><pre><code class="language-json"><span class="token property">"peerDependencies"</span><span class="token operator">:</span> <span class="token punctuation">{</span>
   <span class="token property">"@angular/core"</span><span class="token operator">:</span> <span class="token string">">=19.0.0"</span><span class="token punctuation">,</span>
   <span class="token property">"@angular/common"</span><span class="token operator">:</span> <span class="token string">">=19.0.0"</span>
